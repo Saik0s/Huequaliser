d712009f7911a312dd4f042ae97a6c851318c5f4
Igor Tarasenko <tarasenko.igor.v@gmail.com>
2018-04-28 20:45:40 +0300

    Added LICENSE.md

---
+++ Huequaliser/Sources/Presentation/Scenes/AppFlowController.swift
@@ -0,0 +1,14 @@
+//
+// Copyright © 2018 Igor Tarasenko
+// Licensed under the MIT license, see LICENSE.md file
+//
+import Foundation
+
+///
+/// Why?
+///
+public final class AppFlowController {
+    // MARK: - Public properties
+    // MARK: - Private properties
+    // MARK: - Public methods
+}
